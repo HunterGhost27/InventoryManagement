dd4071e2c4af1982df11bf639dd8d95a52aceb59
Shresht7 <Shresht7@users.noreply.github.com>
2021-03-26 22:56:43 +0530

    Basic Unpack and Repack Functionality

--- Mods/S7_InventoryManagement_5ae0e104-3738-4cf6-8099-7d5b4969065d/Story/RawFiles/Lua/Client/ContextMenu.lua
+++ Mods/S7_InventoryManagement_5ae0e104-3738-4cf6-8099-7d5b4969065d/Story/RawFiles/Lua/Client/ContextMenu.lua
@@ -1,14 +1,49 @@
-Ext.RegisterListener('SessionLoaded', function()
+local unpackedContainers = {}   --  Track Unpacked Containers
+
+Ext.RegisterListener('SessionLoaded', function()    --  Wait for UCL to Load
+
+    --  =========================
+    --  CONTEXT-MENU REGISTRATION
+    --  =========================
+
     UCL.ContextMenu:Register({
         ['Any::Item'] = {
             {
-                text = 'Gather All',
+                text = 'Unpack',
                 actionID = 27801,
-                isUnavailable = function(r) return not r.InputEvents:isPressed('LSHIFT') end,
+                isUnavailable = function(r)
+                    local shiftPressed = r.InputEvents:isPressed('LSHIFT')  --  Shift key pressed
+                    local _, containerInventory = pcall(r.Target.GetInventoryItems, r.Target) -- Check if item is a container
+                    return unpackedContainers[r.Target.MyGuid] or not IsValid(#containerInventory) or not shiftPressed -- Checks if Shift is pressed, Item is a valid container and is not already unpacked
+                end,
+                isLegal = true,
+                isDisabled = false,
+                clickSound = true,
+            },
+            {
+                text = 'Repack',
+                actionID = 27802,
+                isUnavailable = function(r)
+                    local shiftPressed = r.InputEvents:isPressed('LSHIFT')  --  Shift key is pressed
+                    return not unpackedContainers[r.Target.MyGuid] or not shiftPressed -- Checks if shift is pressed and Item is unpacked
+                end,
                 isLegal = true,
                 isDisabled = false,
                 clickSound = true,
             }
         }
     })
+end)
+
+--  ======================
+--  TRACK CONTAINER STATUS
+--  ======================
+
+Ext.RegisterNetListener(UCL.Channel.ContextMenu, function(channel, payload)
+    local payload = Ext.JsonParse(payload) or {}
+    Destringify(payload)
+    if payload.actionID == 27801 or payload.actionID == 27802 then
+        local item = Ext.GetItem(payload.ItemNetID)
+        unpackedContainers[item.MyGuid] = payload.actionID == 27801 and true or false   --  Set unpackedContainer Status
+    end
 end)
---
+++ Mods/S7_InventoryManagement_5ae0e104-3738-4cf6-8099-7d5b4969065d/Story/RawFiles/Lua/Server/ContextMenu.lua
@@ -0,0 +1,37 @@
+local containers = {}
+
+--  =======================
+--  CONTEXT-MENU RESPONDERS
+--  =======================
+
+Ext.RegisterNetListener(UCL.Channel.ContextMenu, function(channel, payload)
+    local payload = Ext.JsonParse(payload) or {}
+    Destringify(payload)
+
+    --  ================
+    --  CONTAINER UNPACK
+    --  ================
+
+    if payload.actionID == 27801 then
+        local item = Ext.GetItem(payload.ItemNetID)
+        if not containers[item.MyGuid] then containers[item.MyGuid] = {} end
+        local containedItems = item:GetInventoryItems()
+        for _, itemName in pairs(containedItems) do
+            containers[item.MyGuid][itemName] = true
+            Osi.ItemToInventory(itemName, payload.CharacterGUID, -1, 1, 0)
+        end
+    end
+
+    --  ================
+    --  CONTAINER REPACK
+    --  ================
+
+    if payload.actionID == 27802 then
+        Ext.Print('repack')
+        local item = Ext.GetItem(payload.ItemNetID)
+        for itemName, _ in pairs(containers[item.MyGuid]) do
+            Osi.ItemToInventory(itemName, item.MyGuid, -1, 1, 0)
+        end
+    containers[item.MyGuid] = nil
+    end
+end)
