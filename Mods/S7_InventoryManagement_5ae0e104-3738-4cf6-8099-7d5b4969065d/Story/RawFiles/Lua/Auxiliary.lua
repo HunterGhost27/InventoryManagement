020f35ad28c449832c8565a21a05d190cd11b3b5
Shresht7 <Shresht7@users.noreply.github.com>
2021-03-26 09:40:00 +0530

    AuxFunctions Init

---
+++ Mods/S7_InventoryManagement_5ae0e104-3738-4cf6-8099-7d5b4969065d/Story/RawFiles/Lua/Auxiliary.lua
@@ -0,0 +1,19 @@
+
+----------------------------------------------------------------
+--==============================================================
+
+IDENTIFIER = 'S7_InventoryManagement'
+
+---@class MODINFO: ModInfo
+---@field ModVersion string
+---@field ModSettings table
+---@field DefaultSettings table
+---@field SubdirPrefix string
+MODINFO = Ext.GetModInfo('5ae0e104-3738-4cf6-8099-7d5b4969065d')
+
+--  ========  AUX FUNCTIONS  ========
+Ext.Require('AuxFunctions/Index.lua')
+--  =================================
+
+--==============================================================
+----------------------------------------------------------------
