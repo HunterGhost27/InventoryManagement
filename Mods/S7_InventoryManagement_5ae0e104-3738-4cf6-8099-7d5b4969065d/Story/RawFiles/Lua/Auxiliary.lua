02732e6d5a34212bd3a632758d3cae87b0900b4a
Shresht7 <Shresht7@users.noreply.github.com>
2021-03-27 12:43:03 +0530

    Update Auxiliary.lua

--- Mods/S7_InventoryManagement_5ae0e104-3738-4cf6-8099-7d5b4969065d/Story/RawFiles/Lua/Auxiliary.lua
+++ Mods/S7_InventoryManagement_5ae0e104-3738-4cf6-8099-7d5b4969065d/Story/RawFiles/Lua/Auxiliary.lua
@@ -26,7 +26,37 @@ PersistentVars = {} --  PersistentVariables
 -- ACTIONS
 -- =======
 ActionID = {
-    Designate = 27801,
-    Unpack = 27802,
-    Repack = 27803,
+    Unpack = 27801,
+    Repack = 27802,
+}
+
+--  ==========
+--  CONTAINERS
+--  ==========
+
+Container = {
+    Accessories = 'LOOT_S7_Container_Accessories',
+    Arrows = 'LOOT_S7_Container_Arrows',
+    BladedWeapons = 'LOOT_S7_Container_BladedWeapons',
+    BluntWeapons = 'LOOT_S7_Container_BluntWeapons',
+    Bombs = 'LOOT_S7_Container_Bombs',
+    Books = 'LOOT_S7_Container_Books',
+    BootsBelts = 'LOOT_S7_Container_BootsBelts',
+    ChestPants = 'LOOT_S7_Container_ChestPants',
+    CoinPursesWares = 'LOOT_S7_Container_CoinPursesWares',
+    Essence = 'LOOT_S7_Container_Essence',
+    Food = 'LOOT_S7_Container_Food',
+    HelmetGloves = 'LOOT_S7_Container_HelmetGloves',
+    Jewelry_corner = 'LOOT_S7_Container_Jewelry',
+    Keys = 'LOOT_S7_Container_Keys',
+    MagicWeapons = 'LOOT_S7_Container_MagicWeapons',
+    Materials = 'LOOT_S7_Container_Materials',
+    Potions = 'LOOT_S7_Container_Potions',
+    QuestItems = 'LOOT_S7_Container_QuestItems',
+    RangedWeapons = 'LOOT_S7_Container_RangedWeapons',
+    Runes = 'LOOT_S7_Container_Runes',
+    Scrolls = 'LOOT_S7_Container_Scrolls',
+    Sellbag = 'LOOT_S7_Container_Sellbag',
+    Shields = 'LOOT_S7_Container_Shields',
+    Skillbooks = 'LOOT_S7_Container_Skillbooks'
 }
