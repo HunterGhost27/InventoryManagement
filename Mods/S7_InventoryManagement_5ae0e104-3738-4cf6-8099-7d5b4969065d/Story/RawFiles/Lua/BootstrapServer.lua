a0172e2635fc9254ecf8db16ed68cdacb42d2983
Shresht7 <Shresht7@users.noreply.github.com>
2021-03-30 01:39:47 +0530

    MCLICK moves item between container and party inventory

--- Mods/S7_InventoryManagement_5ae0e104-3738-4cf6-8099-7d5b4969065d/Story/RawFiles/Lua/BootstrapServer.lua
+++ Mods/S7_InventoryManagement_5ae0e104-3738-4cf6-8099-7d5b4969065d/Story/RawFiles/Lua/BootstrapServer.lua
@@ -8,3 +8,10 @@ Ext.RegisterNetListener(Channel.QuickAdd, function(channel, payload)
     local container = Ext.GetItem(payload.containerNetId)
     Osi.ItemToInventory(item.MyGuid, container.MyGuid, -1, 1, 0)
 end)
+
+Ext.RegisterNetListener(Channel.QuickAdd .. "Return", function(channel, payload)
+    local payload = Ext.JsonParse(payload) or {}
+    if not IsValid(payload) then return end
+    local item = Ext.GetItem(payload.itemNetId)
+    Osi.ItemToInventory(item.MyGuid, Osi.CharacterGetHostCharacter(), -1, 1, 0)
+end)
--- Mods/S7_InventoryManagement_5ae0e104-3738-4cf6-8099-7d5b4969065d/Story/RawFiles/Lua/Client/PartyInventory.lua
+++ Mods/S7_InventoryManagement_5ae0e104-3738-4cf6-8099-7d5b4969065d/Story/RawFiles/Lua/Client/PartyInventory.lua
@@ -1,6 +1,6 @@
 Ext.RegisterListener('InputEvent', function(inputEvent)
     if not IsValid(inputEvent) then return end
-    if inputEvent.EventId == UCL.InputEvents.Alias.LCLICK then
+    if inputEvent.EventId == UCL.InputEvents.Alias.MCLICK then
         local container = UCL.ContainerInventory.Container
         local mousedOverItem = UCL.PartyInventory.MousedOverItem
         if not container or not mousedOverItem then return end
@@ -10,5 +10,19 @@ Ext.RegisterListener('InputEvent', function(inputEvent)
             containerNetId = UCL.ContainerInventory.Container.NetID
         }
         Ext.PostMessageToServer(Channel.QuickAdd, Ext.JsonStringify(payload))
+    end
+end)
+
+Ext.RegisterListener('InputEvent', function(inputEvent)
+    if not IsValid(inputEvent) then return end
+    if inputEvent.EventId == UCL.InputEvents.Alias.MCLICK then
+        local container = UCL.ContainerInventory.MiddleClickedItem
+        if not container then return end
+        local payload = {
+            eventId = inputEvent.EventId,
+            itemNetId = container.NetID,
+            containerNetId = UCL.ContainerInventory.Container.NetID,
+        }
+        Ext.PostMessageToServer(Channel.QuickAdd .. "Return", Ext.JsonStringify(payload))
     end
 end)
